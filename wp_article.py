49c430923f00bd0ff5f1ea6c52b0c6019769ae0a
siznax <steve@siznax.net>
2015-09-16 13:41:43 -0700

    updated docstrings, and privatized _main and _stderr.

--- wp_article.py
+++ wp_article.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 """
-Dump Wikipedia article(s) via Mediawiki API
+Wikipedia article(s) from title(s) via MediaWiki API
 
 INPUT
-    title   "fuzzy" Wiki title
+    titles  "fuzzy" Wiki titles
     format  see FORMATS (default=json)
 
 OUTPUT
-    article in format selected
+    articles in format selected
 
 References
     https://www.mediawiki.org/wiki/API:Main_page
@@ -40,7 +40,7 @@ QUERY = Template(("${API}?titles=${titles}"
                   "&continue="))
 
 
-def stderr(msg):
+def _stderr(msg):
     print(msg, file=sys.stderr)
     sys.stderr.flush()
     sys.stdout.flush()
@@ -52,7 +52,7 @@ def query(titles, _format):
         titles = [titles]
     titles = "|".join([urllib.quote(t) for t in titles])
     qry = QUERY.substitute(API=ENDPOINT, titles=titles, _format=_format)
-    stderr("query: " + qry)
+    _stderr("query: " + qry)
     return qry
 
 
@@ -61,25 +61,26 @@ def dump(title, _format=DEFAULT):
     url = query(title, _format)
     user_agent = "python-requests/" + requests.__version__
     headers = {'User-Agent': user_agent}
-    stderr("request headers: " + str(headers))
+    _stderr("request headers: " + str(headers))
     result = requests.get(url, headers=headers)
-    stderr("status code: " + str(result.status_code))
+    _stderr("status code: " + str(result.status_code))
     return result.text.encode('utf8')
 
 
-def main(title, fmt):
-    print(dump(title, fmt))
+def _main(titles, fmt):
+    print(dump(titles, fmt))
 
 
 if __name__ == "__main__":
     desc = ("Dump Wikipedia article(s) via Mediawiki API\n"
             "https://www.mediawiki.org/wiki/API:Main_page")
     argp = argparse.ArgumentParser(description=desc)
-    argp.add_argument("title", nargs='+',
+    argp.add_argument("titles", nargs='+',
                       help="one or more article titles")
     argp.add_argument("-format", choices=FORMATS, default=DEFAULT,
                       help="output format (default=%s)" % DEFAULT)
     args = argp.parse_args()
+
     start = time.time()
-    main(args.title, args.format)
-    stderr("%5.3f seconds" % (time.time() - start))
+    _main(args.titles, args.format)
+    _stderr("%5.3f seconds" % (time.time() - start))
