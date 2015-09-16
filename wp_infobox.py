7edd7fa3d5fa0db80cc5c9660febc4836be5fbe1
siznax <steve@siznax.net>
2015-09-16 13:43:31 -0700

    MAJOR: added multiple title and dict support.

--- wp_infobox.py
+++ wp_infobox.py
@@ -1,13 +1,12 @@
 #!/usr/bin/env python
 """
-Dump Wikipedia article(s) Infobox wiki-text
+Infobox wiki-text from titles (via API) or file
 
 INPUT
-    title  Wiki article title(s)
-           optionally, local filename
+    Wikipedia article title(s) (or filename)
 
 OUTPUT
-    infobox wiki-text
+    Infobox(es) as wiki-text, dict, or json
 
 References
     https://en.wikipedia.org/wiki/Help:Infobox
@@ -20,21 +19,85 @@ __author__ = "@siznax"
 __version__ = "15 Sep 2015"
 
 import argparse
+import json
 import os
 import re
 import sys
 import time
 import wp_article
 
+from collections import defaultdict
 
-def infobox(wp_txt):
-    """
-    Parse Infobox text from Mediawiki API json output
-    """
+FORMATS = ['dict', 'json', 'text']
+
+def from_api(titles, _format='text'):
+    """returns Infoboxen from api"""
+    return _output(_parse(_articles(titles)), _format)
+
+
+def from_file(fname, _format='text'):
+    """returns Infoboxen from input file"""
+    with open(fname) as fh:
+        return _output(_parse(json.loads(fh.read())), _format)
+
+
+def _output(boxes, _format):
+    """returns Infoboxen as text, dict, or json"""
+    if _format == 'dict':
+        return _boxes_to_dict(boxes)
+    if _format == 'json':
+        return json.dumps(boxes)
+    if _format == 'text':
+        return _boxes_to_text(boxes)
+
+
+def _boxes_to_dict(boxes):
+    """return list of dicts from list of infoboxes"""
+    for i, box in enumerate(boxes):
+        _dict = defaultdict(str)
+        for line in box['wikitext'].split("\n"):
+            _dict['title'] = box['title']
+            if '=' in line:
+                terms = line.split('=')
+                key = terms[0].replace(' ','').replace('|','')
+                _dict[key] = terms[1].strip()
+        boxes[i] = dict(_dict)
+    return boxes
+
+
+def _boxes_to_text(boxes):
+    """return string from list of infoboxes"""
+    text = ""
+    for box in boxes:
+        text += "Title: %s " % box['title']
+        text += box['wikitext'] + "\n"
+    return text
+
+
+def _parse(api_json):
+    """returns [{title, box}] from API JSON"""
+    output = []
+    # print("pages: %d" % len(api_json["query"]['pages']), file=sys.stderr)
+    try:
+        for page in api_json["query"]['pages']:
+            wikitext = page['revisions'][0]["content"]
+            output.append({'title': page['title'],
+                           'wikitext': _infobox(wikitext)})
+    except:
+        raise RuntimeError("Unable to parse result! Check your API query.")
+    return output
+
+
+def _infobox(text):
+    """returns Infobox wiki-text from text blob"""
+    #
+    # TODO: consider returning LIST Of infoboxen (see Abba article)
+    #       instead of the break below...
+    #
     output = []
     region = False
     braces = 0
-    lines = wp_txt.split("\\n")
+    lines = text.split("\n")
     if len(lines) < 3:
         raise RuntimeError("too few lines!")
     for line in lines:
@@ -49,30 +112,44 @@ def infobox(wp_txt):
             output.append(line.lstrip())
             if braces <= 0:
                 region = False
+                break
     return "\n".join(output)
 
 
-def main(args):
-    if os.path.exists(args.title[0]):
-        with open(args.title[0]) as fh:
-            print(infobox(fh.read()))
+def _articles(titles):
+    """returns JSON object from list of titles"""
+    if isinstance(titles, str):
+        titles = [titles]
+    return json.loads(wp_article.dump("|".join(titles)))
+
+
+def _main(titles, _format):
+    """emits Infoboxen from api or local file"""
+    if os.path.exists(titles[0]):
+        return from_file(titles[0], _format)
     else:
-        print(infobox(wp_article.dump("|".join(args.title))))
+        return from_api(titles, _format)
 
 
 if __name__ == "__main__":
     argp = argparse.ArgumentParser(
-        description="Wikipedia article Infobox wiki-text from title(s)")
-    argp.add_argument("title", nargs='+',
-                      help="Wiki article title(s) optionally, local filename")
+        description="Wikipedia article Infobox(es) wiki-text from title(s)")
+    argp.add_argument("titles", nargs='+',
+                      help="article titles (optionally, local filename)")
+    argp.add_argument("-format", choices={'text', 'json'}, default='text',
+                      help="output format (default=text)")
     args = argp.parse_args()
+
     start = time.time()
-    main(args)
+    print(_main(args.titles, args.format).encode('utf-8'))
     print("%5.3f seconds" % (time.time() - start), file=sys.stderr)
 
 
 # test cases TBD
 #
+# Aardvark
+# Abba
+# Accordion
 # Aerocar
 # GitHub
 # Heroku
