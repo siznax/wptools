2502c60ca56b3469a689c975a152865bff21b71f
siznax <steve@siznax.net>
2015-09-28 14:09:10 -0700

    added strip_tags option to wp_get.py

--- wp_get.py
+++ wp_get.py
@@ -7,7 +7,7 @@ INPUT
     input file expected to be Wikipedia HTML
 
 OUTPUT
-    full article (sans boilerplate) or summary only
+    full article (sans boilerplate) or summary (lead_section) only
     as HTML or Markdown text
 
 References
@@ -42,35 +42,43 @@ def _user_agent():
             "Chrome/45.0.2454.85 Safari/537.36")
 
 
-def _process(html, xpath, summary=False):
+def _tostring(elem, strip_tags=False):
+    if strip_tags:
+        return lxml.etree.tostring(elem, method="text", encoding="utf-8")
+    return lxml.etree.tostring(elem)
+
+
+def _process(html, xpath, lead=False, strip_tags=False):
     content = []
     etree = html5lib.parse(html,
                            treebuilder='lxml',
                            namespaceHTMLElements=False)
     root = etree.xpath(xpath)
     for item in root[0]:
-        if summary:
+        if lead:
             if item.tag.startswith("h"):
                 break
             if item.tag == "p":
-                content.append(lxml.etree.tostring(item))
+                content.append(_tostring(item, strip_tags))
         else:
-            content.append(lxml.etree.tostring(item))
+            content.append(_tostring(item, strip_tags))
     return "\n".join(content)
 
 
-def _main(url, sflag, mflag):
+def _main(url, lead, markdown, strip):
     output = ""
     if os.path.exists(url):
         with open(url) as fh:
-            output = _process(fh, XPATH, sflag)
+            output = _process(fh, XPATH, lead, strip)
     else:
         if not args.url.startswith('http'):
-            raise ValueError("'%s' is not file or URL" % url)
+            base = "https://en.wikipedia.org/wiki"
+            url = "%s/%s" % (base, url.replace(" ", "_"))
+        print("GET %s" % url, end=None)
         r = requests.get(url, headers={'user-agent': _user_agent()})
-        print("%s %s" % (url, r.status_code))
-        output = _process(r.content, XPATH, sflag)
-    if mflag:
+        print(r.status_code)
+        output = _process(r.content, XPATH, lead, strip)
+    if markdown:
         print(html2text.html2text(output).encode('utf-8'))
     else:
         print(output)
@@ -80,9 +88,11 @@ if __name__ == "__main__":
     desc = "GET Wikipedia article from URL or filename via HTTP"
     argp = argparse.ArgumentParser(description=desc)
     argp.add_argument("url", help="Wikipedia article URL or filename")
-    argp.add_argument("-s", "-summary", action='store_true',
-                      help="get only lead paragraphs")
+    argp.add_argument("-l", "-lead", action='store_true',
+                      help="lead paragraphs (summary) only")
     argp.add_argument("-m", "-markdown", action='store_true',
                       help="convert content to markdown text")
+    argp.add_argument("-s", "-strip", action='store_true',
+                      help="strip tags")
     args = argp.parse_args()
-    _main(args.url, args.s, args.m)
+    _main(args.url, args.l, args.m, args.s)
