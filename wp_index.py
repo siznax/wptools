7a66db7fa0814c8d8af5d90dc350afaa1b3090c2
siznax <steve@siznax.net>
2015-10-02 09:15:04 -0700

    added -titles to wp_index

--- wp_index.py
+++ wp_index.py
@@ -51,6 +51,7 @@ class IndexParser(WPParser):
     def __init__(self, dest, offset, split):
         WPParser.__init__(self)
         self._files = dict()
+        self._paths = dict()
         self.bytes_read = 0
         self.dest = dest
         self.first_title = ""
@@ -60,6 +61,8 @@ class IndexParser(WPParser):
         self.tell = 0
         self.title = ""
         self.title_start = 0
+        self.titles = None
+        self.titles_written = 0
 
     def open_files(self):
         if not self.dest:
@@ -69,15 +72,38 @@ class IndexParser(WPParser):
             path = "%s/%s" % (self.dest, char)
             print("+ open %s" % path)
             self._files[char] = bz2.BZ2File(path, 'w')
+            self._paths[char] = path
+        lpath = self.dest + "_left"
+        print("+ open %s" % lpath)
+        self._files['leftover'] = file(lpath, 'w')
+        self._paths['leftover'] = lpath
 
     def close_files(self):
         if not self.dest:
             return
-        for path, handle in self._files.iteritems():
+        for key, handle in self._files.iteritems():
             tell = handle.tell()
+            path = self._paths[key]
             handle.close()
             if tell:
-                print("- close %s %d" % (path, tell))
+                print("wrote %d bytes to %s" % (tell, path))
+
+    def _write(self, title, title_start, elem):
+        first_char = ascii_bin(title)
+        if first_char in self._files:
+            _file = self._files[first_char]
+            if self.split:
+                if self.titles:
+                    if title in self.titles:
+                        _file.write(elem)
+                        self.titles_written += 1
+                        self.titles.remove(title)
+                else:
+                    _file.write(elem)
+            else:
+                _file.write("%s %s\n" % (title, title_start))
+        else:
+            print("ORPHAN %s" % title)
 
     def process(self, elem):
         title = page_title(elem)
@@ -90,15 +116,7 @@ class IndexParser(WPParser):
         if not self.dest:
             # print("%s %s" % (title, len(elem)))
             return
-        first_char = ascii_bin(title)
-        if first_char in self._files:
-            _file = self._files[first_char]
-            if self.split:
-                _file.write(elem)
-            else:
-                _file.write("%s %s\n" % (title, title_start))
-        else:
-            print("ORPHAN %s" % title)
+        self._write(title, title_start, elem)
 
 
 def page_title(page):
@@ -119,12 +137,16 @@ def ascii_bin(title):
         return title
 
 
-def setup(dest, offset, split):
-    if os.path.exists(dest):
+def setup(dest, offset, split, titles):
+    if dest and os.path.exists(dest):
         print("Destination exists: %s" % dest, file=sys.stderr)
         sys.exit(os.EX_IOERR)
     ip = IndexParser(dest, offset, split)
     ip.open_files()
+    if titles:
+        with open(titles) as fh:
+            ip.titles = set(fh.read().split("\n"))
+            print("Pulling %d titles" % len(ip.titles), file=sys.stderr)
     return ip
 
 
@@ -149,17 +171,25 @@ def gobble(ip, fname, chunk_size, max_mb, offset):
 
 
 def teardown(ip):
+    ip._files['leftover'].write("\n".join(sorted(ip.titles)))
     ip.close_files()
     print("pages found: %d" % ip.elems_found)
     print("titles processed: %d" % ip.elems_processed)
+    print("titles written: %d" % ip.titles_written)
+    print("titles leftover: %d" % len(ip.titles))
     print("first: %s %d" % (ip.first_title, ip.first_title_start))
     print("last: %s %d" % (ip.title, ip.title_start))
     print("read: %d MB" % (ip.bytes_read / ONE_MB))
     print("tell: %s" % ip.tell)
 
 
-def _main(fname, max_mb, chunk_size, dest, offset, split):
-    ip = setup(dest, offset, split)
+def _main(fname, max_mb, chunk_size, dest, offset, split, titles):
+    if titles:
+        split = True
+    if titles and not dest:
+        print("-titles doesn't make sense without -dest")
+        sys.exit(os.EX_USAGE)
+    ip = setup(dest, offset, split, titles)
     gobble(ip, fname, chunk_size, max_mb, offset)
     teardown(ip)
 
@@ -170,16 +200,16 @@ if __name__ == "__main__":
     argp.add_argument("fname", help="XML Dump bz2 filename")
     argp.add_argument("-c", "-chunksize", type=int, default=DEFAULT_CHUNK_KB,
                       help="chunk size in KB (default=%d)" % DEFAULT_CHUNK_KB)
-    argp.add_argument("-d", "-dest", default="",
-                      help="write results to dest (dir)")
+    argp.add_argument("-d", "-dest", help="write results to dest (dir)")
     argp.add_argument("-m", "-maxbytes", type=int, default=MAX_MEGABYTES,
                       help="max bytes in MB (default=%d)" % MAX_MEGABYTES)
     argp.add_argument("-o", "-offset", type=int, default=0,
                       help="seek to byte offset")
     argp.add_argument("-s", "-split", action='store_true',
                       help="split (instead of index) into files")
+    argp.add_argument("-t", "-titles", help="flat file of titles to pull")
     args = argp.parse_args()
 
     start = time.time()
-    _main(args.fname, args.m, args.c, args.d, args.o, args.s)
+    _main(args.fname, args.m, args.c, args.d, args.o, args.s, args.t)
     print("%5.3f seconds" % (time.time() - start))
