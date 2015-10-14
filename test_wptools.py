99d44d25475c695564a5ed6b98707352e898007f
siznax <steve@siznax.net>
2015-10-13 18:16:47 -0700

    added unit tests stub.

---
+++ test_wptools.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+"""Tests for WPTools."""
+
+import unittest
+import wptools
+
+
+class WPToolsTestCase(unittest.TestCase):
+
+    def test_entry_points(self):
+
+        wptools.extract
+        wptools.fetch
+        wptools.utils
+
+
+class ExtractTestCase(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+class FetchTestCase(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+class UtilsTestCase(unittest.TestCase):
+
+    def test(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
