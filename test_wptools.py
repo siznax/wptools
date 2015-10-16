0bc95d753bc62e57cfa7c6df30b6d33f0e0e0a84
siznax <steve@siznax.net>
2015-10-15 17:13:11 -0700

    added test comments.

--- test_wptools.py
+++ test_wptools.py
@@ -18,13 +18,12 @@ class WPToolsTestCase(unittest.TestCase):
 
 class ExtractTestCase(unittest.TestCase):
 
-    # Potential Test Cases
-    # Active galactic nucleus
-    # Ag Qoyunlu
-    # Dhyan Chand
-    # Einstein
+    # 14th Dalai Lama
 
     def test_html_lead(self):
+        # Ag Qoyunlu
+        # Dhyan Chand
+        # Einstein
         from wptools.extract import html_lead
         data = ("<p>1</p><table/>"
                 "<p>2</p>"
@@ -33,6 +32,7 @@ class ExtractTestCase(unittest.TestCase):
         self.assertEqual(ans, "<p>1</p>\n<p>2</p>\n<p>3</p>")
 
     def test_html_lead_no_templates(self):
+        # Active galactic nucleus
         from wptools.extract import html_lead
         ans = html_lead("<p>lead</p>")
         self.assertEqual(ans, "<p>lead</p>")
