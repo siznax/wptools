bd63b686beb5ada3c2a111ea37a40f313c08f000
siznax <steve@siznax.net>
2016-08-10 21:11:44 -0700

    added wptools.api module.

---
+++ wptools/api.py
@@ -0,0 +1,56 @@
+# -*- coding:utf-8 -*-
+
+"""
+wptools.api
+~~~~~~~~~~~
+
+This is the WPTools API.
+"""
+
+from . import extract
+from . import fetch
+
+
+def html(title, lead=False, test=False, verbose=False,
+         wiki=fetch.WPToolsFetch.ENDPOINT):
+    """returns article HTML"""
+    data = fetch.get_html(title, lead, test, wiki, verbose)
+    if test:
+        return data
+    return extract.qry_html(data, lead)
+
+
+def infobox(title, test=False, verbose=False,
+            wiki=fetch.WPToolsFetch.ENDPOINT):
+    """returns article Infobox as dict"""
+    data = fetch.get_parsetree(title, False, test, wiki, verbose)
+    if test:
+        return data
+    return extract.qry_infobox(data, 'dict')
+
+
+def parsetree(title, lead=False, test=False,
+              wiki=fetch.WPToolsFetch.ENDPOINT):
+    """returns article parse tree as XML"""
+    data = fetch.get_parsetree(title, lead, test, wiki)
+    if test:
+        return data
+    return extract.qry_parsetree(data)
+
+
+def text(title, compact=False, lead=False, test=False, verbose=False,
+         wiki=fetch.WPToolsFetch.ENDPOINT):
+    """returns article as Markdown text"""
+    data = fetch.get_html(title, lead, test, wiki, verbose)
+    if test:
+        return data
+    return extract.qry_text(data, lead, compact)
+
+
+def wikitext(title, lead=False, test=False,
+             wiki=fetch.WPToolsFetch.ENDPOINT):
+    """return article wikitext"""
+    data = fetch.get_wikitext(title, lead, test, wiki)
+    if test:
+        return data
+    return extract.qry_wikitext(data)
