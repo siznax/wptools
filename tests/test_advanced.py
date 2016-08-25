5ca8643c08b8fd1841d0b1264cdab1e7efcd7203
siznax <steve@siznax.net>
2016-08-25 14:30:46 -0700

    added development tests

---
+++ tests/test_advanced.py
@@ -0,0 +1,99 @@
+# -*- coding:utf-8 -*-
+
+import wptools
+
+
+# WPTOOLS ADVANCED TESTS
+# ======================
+#
+# See also ./scripts/wptool.py (no arguments does random everything)
+
+
+# STRESS TESTS
+# ------------
+# These are good in development, making actual HTTP requests.
+
+# Get random item everything
+# EXPECT: no breakage
+#
+wptools.wptools().get()
+
+# Get everything from random item in another language
+# EXPECT: no breakage
+#
+# wptools.wptools(lang='ja').get()
+
+# Get random item from another wiki
+# EXPECT: no breakage
+#
+# wptools.wptools(wiki='en.wikinews.org').get()
+
+
+# FULLY HYDRATE
+# -------------
+# These can be done with simulated HTTP responses. 
+
+# Get specific title
+# EXPECT: should populate everything
+#
+# wptools.wptools('Shakespeare').get()
+
+# Get specific title w/redirect
+# EXPECT: should populate everything
+#
+# wptools.wptools(title='Abe Lincoln').get()
+
+# Get everything in another language
+# EXPECT: should populate everything
+#
+# wptools.wptools('Napoleon', lang='fr').get()
+
+# Get everything wikibase only
+# Q43303 Malcolm_X
+# EXPECT: should populate everything
+#
+# wptools.wptools(wikibase='Q43303').get()
+
+# Get everything multibyte request
+# e.g. 托爾金 (zh) Tolkien
+# EXPECT: should populate everything
+#
+# wptools.wptools('托爾金', lang='zh')
+
+# Get everything mixed languages
+# EXPECT: should populate everything
+#
+# wptools.wptools(title='Abe Lincoln', lang='zh').get()
+
+
+# COMPLEX INFOBOXEN
+# -----------------
+# Just need to supply a tmp/parsetree.xml
+
+# Successfully populate complex infobox dict
+# EXPECT: <dict(42)>
+#
+# wptools.wptools('Abe Lincoln').get()
+   
+
+# BAD REQUESTS
+# ------------
+# More failed requests needed here.
+
+# Mediawiki site function not supported
+# e.g. "jp" Wikinews (unknown language code)
+# EXPECT: error message, self.fatal = True
+#
+# wptools.wptools(wiki='jp.wikinews.org').get()
+
+
+# Random Points of Interest
+# -------------------------
+# Need more analysis.
+
+# no image, no infobox
+# wptools.wptools('audio mining').get()
+
+# has country (P17) and point in time (P585)
+# wptools.wptools(wikibase='Q34664').get_wikidata()
+# print h.g_wikidata['query']
